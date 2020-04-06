4aaeea58965011dcf09b1fd5507c75264d916f55
Xiaosong Gao <gxsshallot@163.com>
2020-04-06 10:01:29 +0800

    添加挂载卷的读写模式常量。

--- constant.go
+++ constant.go
@@ -18,4 +18,8 @@ const (
 	RestartAlways        = "always"         // 总是重启容器
 	RestartOnFailure     = "on-failure"     // 如果退出时的错误码是错误的话，重启容器
 	RestartUnlessStopped = "unless-stopped" // 只有停止了要重启容器
+
+	// 挂载卷的读写模式
+	VolumeReadOnly  = "ro" // 只读模式
+	VolumeReadWrite = "rw" // 读写模式
 )
